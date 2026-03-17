f97e232518d454092432e0214668cf3cc338cfe5
piere steven <ingenieropshp@gmail.com>
2026-03-17 15:57:48 -0500

    le quite el dado del buscardor

---
+++ src/restaurantes.js
@@ -0,0 +1,32 @@
+// restaurantes.js
+export const listaRestaurantes = [
+  { 
+    id: 1, 
+    nombre: "101 Bistró", 
+    tipo: "Gourmet", 
+    telefono: "573001234567", // Número de ejemplo
+    img: "https://pngimg.com/uploads/sandwich/sandwich_PNG48.png" 
+  },
+  { 
+    id: 2, 
+    nombre: "Mundo Marino", 
+    tipo: "Mariscos", 
+    telefono: "573007654321", 
+    img: "https://pngimg.com/uploads/pancake/pancake_PNG8.png" 
+  },
+  // Añade el teléfono a los demás...
+  { 
+    id: 2, 
+    nombre: "Mundo Marino", 
+    tipo: "Mariscos", 
+    telefono: "573007654321", 
+    img: "https://pngimg.com/uploads/pancake/pancake_PNG8.png" 
+  },
+  { 
+    id: 2, 
+    nombre: "Mundo Marino", 
+    tipo: "Mariscos", 
+    telefono: "573007654321", 
+    img: "https://pngimg.com/uploads/pancake/pancake_PNG8.png" 
+  },
+];
